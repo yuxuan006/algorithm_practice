3f961e989899d2f77d53ad50884922c3473c6a44
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-04-11 01:52:40 -0400

    revised

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
-using namespace std
+using namespace std;
+
 int binarySearch(int a[], int first, int last, int value){
+    int index;
+    if(first > last){
+        index = -1;
+    }
     int mid = (first+last)/2;
     
     if(a[mid] == value){
-        return mid;
+        cout<<"num: "<<a[mid]<<endl;
+        index = mid;
+    }
+    else if(a[mid] > value){
+        cout<<"num: "<<a[mid]<<endl;
+        index = binarySearch(a, first, mid-1, value);
     }
     else{
-        if(a[mid] < value){
-            mid = binarySearch(a, 0, mid-1, value);
-        }
-        else{
-            mid = binarySearch(a, mid+1, last, value);
-        }
+        cout<<"num: "<<a[mid]<<endl;
+        index = binarySearch(a, mid+1, last, value);
     }
+    return index;
 }
 int main()
 {
-    int a[] = {1,2,3};
-    int num = binarySearch(a, 0, 2, 2);
+    int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    //for(int i = 1; i <= 20; i++){
+    // a[i-1] = i;
+    //}
+    //int a[] = {1,2,3,4,5,6};
+    int num = binarySearch(a, 0, 19, 9);
     cout<<num<<endl;
     return 0;
 }
+
