eac587e79b960f5d2991a402098cbc086d47f0f8
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-04-17 13:23:54 -0400

    heap sort

--- convert_int_array_to_string.cpp
+++ convert_int_array_to_string.cpp
@@ -8,3 +8,15 @@ string int_array_to_string(int a[], int size)
     }
     return s;
 }
+
+//convert int to string(not using to_string())
+
+string int_to_string(int x){
+    string s;
+    while(x > 0){
+        int n = x%10;
+        s += n + '0';
+        x = x/10;
+    }
+    return s;
+}
