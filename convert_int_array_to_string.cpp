bd66ae35522e46d34c9d22a34273037d3793ab78
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-06-07 14:47:48 -0400

    modified

--- convert_int_array_to_string.cpp
+++ convert_int_array_to_string.cpp
@@ -11,16 +11,34 @@ string int_array_to_string(int a[], int size)
 
 //convert int to string(not using to_string())
 
-string int_to_string(int x){
-    string st;
-    if(x == 0)return "0";
-    while(x > 0){
-        int n = x%10;
-        st += n + '0';
-        x = x/10;
+#include <string>
+using namespace std;
+string inttostring(int n){
+    string ret = "";
+    int i = 0, x;
+    int flag = 1;
+    if(n < 0){
+        flag = -1;
+        n *= -1;
     }
-    if (x < 0) {
-        st = "-"+st;
+    while(n > 0){
+        x = n % 10;
+        ret.insert(0, 1, x + '0');
+        n = n/10;
     }
-    return st;
+    if(flag == -1){
+        ret.insert(0, 1, '-');
+        return ret;
+    }
+    return ret;
+}
+using namespace std;
+
+int main()
+{
+    int i = -124;
+    string s = inttostring(i);
+    cout <<s<< endl;
+    
+    return 0;
 }
